2079ab1fb88132254536ab23a458d58c42b20b78
Will Taylor <wrftaylor@gmail.com>
2022-07-20 13:28:03 +0100

    Remove code

--- main.tf
+++ main.tf
@@ -7,9 +7,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-# resource "azurerm_resource_group" "rg" {
-#   name     = "${var.product}-${var.env}"
-#   location = var.location
-#   tags     = var.common_tags
-# }
